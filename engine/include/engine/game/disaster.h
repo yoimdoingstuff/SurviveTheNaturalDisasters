1c853a18a8764b690285025cd1ed718f9e19c4ae
yoimdoingstuff <leafoundas@outlook.com>
2026-09-15 21:18:07 +0930

    Add earthquake disaster interface

---
+++ engine/include/engine/game/disaster.h
@@ -0,0 +1,20 @@
+#ifndef NDS_ENGINE_GAME_DISASTER_H
+#define NDS_ENGINE_GAME_DISASTER_H
+
+#include "engine/game/player.h"
+#include <stdint.h>
+
+typedef struct nds_earthquake {
+    float elapsed;
+    float pulse_timer;
+    float pulse_interval;
+    uint32_t pulse_count;
+    uint8_t active;
+} nds_earthquake;
+
+void nds_earthquake_init(nds_earthquake* earthquake);
+void nds_earthquake_start(nds_earthquake* earthquake);
+void nds_earthquake_stop(nds_earthquake* earthquake);
+void nds_earthquake_update(nds_earthquake* earthquake, nds_player_controller* player, float dt);
+
+#endif
