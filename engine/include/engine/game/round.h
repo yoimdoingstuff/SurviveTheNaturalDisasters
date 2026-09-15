77762ce07e19f38c480fb25a6f3b1568e771d8ad
yoimdoingstuff <leafoundas@outlook.com>
2026-09-15 21:08:58 +0930

    Add basic NDS round state API

---
+++ engine/include/engine/game/round.h
@@ -0,0 +1,26 @@
+#ifndef NDS_ENGINE_GAME_ROUND_H
+#define NDS_ENGINE_GAME_ROUND_H
+
+#include <stdint.h>
+
+typedef enum nds_round_state {
+    NDS_ROUND_INTERMISSION = 0,
+    NDS_ROUND_PLAYING = 1,
+    NDS_ROUND_RESULTS = 2
+} nds_round_state;
+
+typedef struct nds_round {
+    nds_round_state state;
+    float state_time;
+    float intermission_duration;
+    float round_duration;
+    float results_duration;
+    uint32_t round_number;
+    uint8_t player_survived;
+} nds_round;
+
+void nds_round_init(nds_round* round);
+void nds_round_update(nds_round* round, float delta_seconds);
+const char* nds_round_state_name(nds_round_state state);
+
+#endif
