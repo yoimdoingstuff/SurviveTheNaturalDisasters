70edd533590dff8e1300141c219db699508f95f8
yoimdoingstuff <leafoundas@outlook.com>
2026-09-15 21:23:16 +0930

    Add disaster selection to round state

--- engine/include/engine/game/round.h
+++ engine/include/engine/game/round.h
@@ -9,6 +9,10 @@ typedef enum nds_round_state {
     NDS_ROUND_RESULTS = 2
 } nds_round_state;
 
+typedef enum nds_disaster_type {
+    NDS_DISASTER_EARTHQUAKE = 0
+} nds_disaster_type;
+
 typedef struct nds_round {
     nds_round_state state;
     float state_time;
@@ -17,10 +21,12 @@ typedef struct nds_round {
     float results_duration;
     uint32_t round_number;
     uint8_t player_survived;
+    nds_disaster_type disaster;
 } nds_round;
 
 void nds_round_init(nds_round* round);
 void nds_round_update(nds_round* round, float delta_seconds);
 const char* nds_round_state_name(nds_round_state state);
+const char* nds_disaster_type_name(nds_disaster_type disaster);
 
 #endif
