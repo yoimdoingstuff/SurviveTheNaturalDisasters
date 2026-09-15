60ef9c4cffa6890485618b2e69f3edad41dd9e0e
yoimdoingstuff <leafoundas@outlook.com>
2026-09-15 21:21:12 +0930

    Expand earthquake to affect scene parts

--- engine/include/engine/game/disaster.h
+++ engine/include/engine/game/disaster.h
@@ -2,12 +2,15 @@
 #define NDS_ENGINE_GAME_DISASTER_H
 
 #include "engine/game/player.h"
+#include "engine/scene/instance.h"
 #include <stdint.h>
 
 typedef struct nds_earthquake {
     float elapsed;
     float pulse_timer;
     float pulse_interval;
+    float previous_shake_x;
+    float previous_shake_z;
     uint32_t pulse_count;
     uint8_t active;
 } nds_earthquake;
@@ -15,6 +18,7 @@ typedef struct nds_earthquake {
 void nds_earthquake_init(nds_earthquake* earthquake);
 void nds_earthquake_start(nds_earthquake* earthquake);
 void nds_earthquake_stop(nds_earthquake* earthquake);
-void nds_earthquake_update(nds_earthquake* earthquake, nds_player_controller* player, float dt);
+void nds_earthquake_update(nds_earthquake* earthquake, nds_player_controller* player,
+                           nds_instance* scene, float dt);
 
 #endif
