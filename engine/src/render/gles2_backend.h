2aa55b060e7e3f31b4abe80ef977544405f75ccd
yoimdoingstuff <leafoundas@outlook.com>
2026-09-16 13:08:25 +0930

    Add HUD backend interface

--- engine/src/render/gles2_backend.h
+++ engine/src/render/gles2_backend.h
@@ -4,6 +4,7 @@
 #include "engine/core/types.h"
 #include "engine/render/draw.h"
 #include "engine/render/camera.h"
+#include "engine/render/gles2.h"
 
 typedef struct nds_gles2_backend nds_gles2_backend;
 
@@ -15,6 +16,8 @@ nds_result nds_gles2_backend_begin(nds_gles2_backend* backend);
 nds_result nds_gles2_backend_draw_parts(nds_gles2_backend* backend,
                                         const nds_draw_list* list,
                                         const nds_camera* camera);
+nds_result nds_gles2_backend_draw_hud(nds_gles2_backend* backend,
+                                      const nds_hud_state* state);
 nds_result nds_gles2_backend_end(nds_gles2_backend* backend);
 
 #endif
